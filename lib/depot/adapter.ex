fa3eea8978be0c056b74b9393766c4e9b19a2ec1
Michael Crumm <mike@crumm.net>
2020-07-09 16:07:31 -0700

    expose stream opts to adapter

--- lib/depot/adapter.ex
+++ lib/depot/adapter.ex
@@ -3,13 +3,14 @@ defmodule Depot.Adapter do
   Behaviour for how `Depot` adapters work.
   """
   @type path :: Path.t()
+  @type stream_opts :: keyword
   @opaque config :: struct
 
   @callback starts_processes() :: boolean
   @callback configure(keyword) :: {module(), config}
   @callback write(config, path, contents :: iodata()) :: :ok | {:error, term}
   @callback read(config, path) :: {:ok, binary} | {:error, term}
-  @callback read_stream(config, path) :: {:ok, Enumerable.t()} | {:error, term}
+  @callback read_stream(config, path, stream_opts) :: {:ok, Enumerable.t()} | {:error, term}
   @callback delete(config, path) :: :ok | {:error, term}
   @callback move(config, source :: path, destination :: path) :: :ok | {:error, term}
   @callback copy(config, source :: path, destination :: path) :: :ok | {:error, term}
