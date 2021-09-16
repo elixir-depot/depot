34219eef49d1b566a6d3f624fb4f54447d09630e
Benjamin Milde <benni@kobrakai.de>
2021-09-16 20:51:27 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Depot.MixProject do
   def project do
     [
       app: :depot,
-      version: "0.5.1",
+      version: "0.5.2",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
