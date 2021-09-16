6fb56be785e07521963311b891f6cabe7fcfc960
Benjamin Milde <benni@kobrakai.de>
2021-09-16 20:51:00 +0200

    Make module based filesystems runtime configurable

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,8 +5,12 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+
+
+## [0.5.2] - 2021-09-16
 ### Added
-- Added visibility handling with callbacks and converters
+- Added WIP visibility handling with callbacks and converters
+- Runtime configuration for filesystems via the app env
 
 
 ## [0.5.1] - 2021-09-12
--- lib/depot/filesystem.ex
+++ lib/depot/filesystem.ex
@@ -21,70 +21,81 @@ defmodule Depot.Filesystem do
   defmacro __using__(opts) do
     quote bind_quoted: [opts: opts] do
       @behaviour Depot.Filesystem
-
       {adapter, opts} = Depot.Filesystem.parse_opts(__MODULE__, opts)
-      opts = Keyword.put_new(opts, :name, __MODULE__)
-      @filesystem adapter.configure(opts)
+      @adapter adapter
+      @opts opts
+      @key {Depot.Filesystem, __MODULE__}
 
-      if adapter.starts_processes() do
-        def child_spec(_) do
-          Supervisor.child_spec(@filesystem, %{})
-        end
+      def init do
+        filesystem =
+          @opts
+          |> Depot.Filesystem.merge_app_env(__MODULE__)
+          |> @adapter.configure()
+
+        :persistent_term.put(@key, filesystem)
+
+        filesystem
       end
 
       def __filesystem__ do
-        @filesystem
+        :persistent_term.get(@key, init())
+      end
+
+      if adapter.starts_processes() do
+        def child_spec(_) do
+          Supervisor.child_spec(__filesystem__(), %{})
+        end
       end
 
       @impl true
       def write(path, contents, opts \\ []),
-        do: Depot.write(@filesystem, path, contents, opts)
+        do: Depot.write(__filesystem__(), path, contents, opts)
 
       @impl true
       def read(path, opts \\ []),
-        do: Depot.read(@filesystem, path, opts)
+        do: Depot.read(__filesystem__(), path, opts)
 
       @impl true
       def read_stream(path, opts \\ []),
-        do: Depot.read_stream(@filesystem, path, opts)
+        do: Depot.read_stream(__filesystem__(), path, opts)
 
       @impl true
       def delete(path, opts \\ []),
-        do: Depot.delete(@filesystem, path, opts)
+        do: Depot.delete(__filesystem__(), path, opts)
 
       @impl true
       def move(source, destination, opts \\ []),
-        do: Depot.move(@filesystem, source, destination, opts)
+        do: Depot.move(__filesystem__(), source, destination, opts)
 
       @impl true
       def copy(source, destination, opts \\ []),
-        do: Depot.copy(@filesystem, source, destination, opts)
+        do: Depot.copy(__filesystem__(), source, destination, opts)
 
       @impl true
       def file_exists(path, opts \\ []),
-        do: Depot.file_exists(@filesystem, path, opts)
+        do: Depot.file_exists(__filesystem__(), path, opts)
 
       @impl true
       def list_contents(path, opts \\ []),
-        do: Depot.list_contents(@filesystem, path, opts)
+        do: Depot.list_contents(__filesystem__(), path, opts)
     end
   end
 
   def parse_opts(module, opts) do
-    if Keyword.has_key?(opts, :otp_app) do
-      otp_app = Keyword.fetch!(opts, :otp_app)
-      config = Application.get_env(otp_app, module, [])
-      adapter = opts[:adapter] || config[:adapter]
+    opts
+    |> merge_app_env(module)
+    |> Keyword.put_new(:name, module)
+    |> Keyword.pop!(:adapter)
+  end
 
-      unless adapter do
-        raise ArgumentError,
-              "missing :adapter configuration in " <>
-                "config #{inspect(otp_app)}, #{inspect(module)}"
-      end
+  def merge_app_env(opts, module) do
+    case Keyword.fetch(opts, :otp_app) do
+      {:ok, otp_app} ->
+        config = Application.get_env(otp_app, module, [])
+        Keyword.merge(opts, config)
 
-      {adapter, config}
-    else
-      Keyword.pop!(opts, :adapter)
+      :error ->
+        opts
     end
   end
 end
