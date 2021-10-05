033e8d6203c873bc0a9a1f37f5819375431dee75
Jason Schweier <jasons@nulogy.com>
2021-10-05 12:08:16 -0400

    Bump Elixir to at least 1.11

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Depot.MixProject do
     [
       app: :depot,
       version: "0.5.1",
-      elixir: "~> 1.10",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
